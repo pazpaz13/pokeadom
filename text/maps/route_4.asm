d2cd01941b5e1ea1b9c42e80182db96d5bd6940c
lalukon <65565943+lalukon@users.noreply.github.com>
2020-05-18 22:42:17 +0300

    traslate route_4.asm

--- text/maps/route_4.asm
+++ text/maps/route_4.asm
@@ -1,36 +1,36 @@
 _Route4Text1:: ; 8db1e (23:5b1e)
-	text "Ouch! I tripped"
-	line "over a rocky"
-	cont "#MON, GEODUDE!"
+	text "אווץ'! מעדתי על"
+	line "#ימון סלעי במיוחד"
+	cont "GEODUDE!" ; TODO
 	done
 
 _Route4BattleText1:: ; 8db4b (23:5b4b)
-	text "I came to get my"
-	line "mushroom #MON!"
+	text "הגעתי לקחת"
+	line "את#ימון הפטרייה שלי!"
 	done
 
 _Route4EndBattleText1:: ; 8db6c (23:5b6c)
-	text "Oh! My cute"
-	line "mushroom #MON!"
+	text "הו! #ימון פטרייה"
+	line "מתוק שלי!"
 	prompt
 
 _Route4AfterBattleText1:: ; 8db88 (23:5b88)
-	text "There might not"
-	line "be any more"
-	cont "mushrooms here."
+	text "יכול להיות"
+	line "שלא נותרה כאן"
+	cont "אף פטרייה."
 
-	para "I think I got"
-	line "them all."
+	para "אני חושבת שתפסתי"
+	line "את כולן."
 	done
 
 _Route4Text5:: ; 8dbcd (23:5bcd)
-	text "MT.MOON"
-	line "Tunnel Entrance"
+	text "הר הירח"
+	line "מעבר המנהרה"
 	done
 
 _Route4Text6:: ; 8dbe6 (23:5be6)
-	text "ROUTE 4"
-	line "MT.MOON -"
-	cont "CERULEAN CITY"
+	text "דרך 4"
+	line "הר הירח -"
+	cont "עיר הטורקיז"
 	done
 

c7678a046612c3b195b95b8d14bf644e1d80aabd
lalukon <65565943+lalukon@users.noreply.github.com>
2020-05-18 22:31:15 +0300

    translate route_3.asm

--- text/maps/route_3.asm
+++ text/maps/route_3.asm
@@ -1,146 +1,146 @@
 _Route3Text1:: ; 8d779 (23:5779)
-	text "Whew... I better"
-	line "take a rest..."
-	cont "Groan..."
+	text "פיו.. כדאי שאני.."
+	line "אנוח קצת.."
+	cont "אנחה..."
 
-	para "That tunnel from"
-	line "CERULEAN takes a"
-	cont "lot out of you!"
+	para "המנהרה הזאת"
+	line "מתעלת עיר הטורקיז"
+	cont "ממש מוציאה ממך את המיץ!"
 	done
 
 _Route3BattleText1:: ; 8d7d5 (23:57d5)
-	text "Hey! I met you in"
-	line "VIRIDIAN FOREST!"
+	text "היי! אני פגשתי אותך"
+	line "ביער ירוק-עד!"
 	done
 
 _Route3EndBattleText1:: ; 8d7f9 (23:57f9)
-	text "You"
-	line "beat me again!"
+	text "אני לא מאמין!"
+	line "ניצחת אותי שוב!"
 	prompt
 
 _Route3AfterBattleText1:: ; 8d80d (23:580d)
-	text "There are other"
-	line "kinds of #MON"
-	cont "than those found"
-	cont "in the forest!"
+	text "ישנם סוגים"
+	line "נוספים של #ימונים"
+	cont "מלבד אלו"
+	cont "שמצאת ביער!"
 	done
 
 _Route3BattleText2:: ; 8d84c (23:584c)
-	text "Hi! I like shorts!"
-	line "They're comfy and"
-	cont "easy to wear!"
+	text "היי! אני אוהב מכנסיים קצרים"
+	line "הם נוחים וגם"
+	cont "קלים ללבישה!"
 	done
 
 _Route3EndBattleText2:: ; 8d87f (23:587f)
-	text "I don't"
-	line "believe it!"
+	text "אני לא"
+	line "מאמין לזה!"
 	prompt
 
 _Route3AfterBattleText2:: ; 8d893 (23:5893)
-	text "Are you storing"
-	line "your #MON on"
-	cont "PC? Each BOX can"
-	cont "hold 20 #MON!"
+	text "אתה מאכסן"
+	line "את ה#ימונים שלך"
+	cont "במחשב? כל קופסא"
+	cont "יכולה להחזיק 20 #ימונים!"
 	done
 
 _Route3BattleText3:: ; 8d8d0 (23:58d0)
-	text "You looked at me,"
-	line "didn't you?"
+	text "אתה הסתכלת עליי"
+	line "נכון?"
 	done
 
 _Route3EndBattleText3:: ; 8d8ee (23:58ee)
-	text "You're"
-	line "mean!"
+	text "אתה"
+	line "רשע מרושע!"
 	prompt
 
 _Route3AfterBattleText3:: ; 8d8fb (23:58fb)
-	text "Quit staring if"
-	line "you don't want to"
-	cont "fight!"
+	text "תפסיק לבהות"
+	line "אם אתה לא רוצה"
+	cont "להלחם!"
 	done
 
 _Route3BattleText4:: ; 8d924 (23:5924)
-	text "Are you a trainer?"
-	line "Let's fight!"
+	text "אתה מאמן פוקימונים?"
+	line "בוא נלחם!"
 	done
 
 _Route3EndBattleText4:: ; 8d944 (23:5944)
-	text "If I"
-	line "had new #MON I"
-	cont "would've won!"
+	text "לו היה לי"
+	line "#מון חדש"
+	cont "הייתי מנצחת!"
 	prompt
 
 _Route3AfterBattleText4:: ; 8d966 (23:5966)
-	text "If a #MON BOX"
-	line "on the PC gets"
-	cont "full, just switch"
-	cont "to another BOX!"
+	text "אם קופסאת #ימונים"
+	line "במחשב מלאה עד אפס מקום,"
+	cont "פשוט תחליף"
+	cont "לקופסא אחרת!"
 	done
 
 _Route3BattleText5:: ; 8d9a6 (23:59a6)
-	text "That look you"
-	line "gave me, it's so"
-	cont "intriguing!"
+	text "הצורה שבה הבטת בי"
+	line "פשוט כל כך"
+	cont "מסקרנת!"
 	done
 
 _Route3EndBattleText5:: ; 8d9d1 (23:59d1)
-	text "Be nice!"
+	text "תהיה נחמד!"
 	prompt
 
 _Route3AfterBattleText5:: ; 8d9db (23:59db)
-	text "Avoid fights by"
-	line "not letting"
-	cont "people see you!"
+	text "כדי להמנע מקרבות,"
+	line "אל תיתן"
+	cont "לאנשים לראות אותך!"
 	done
 
 _Route3BattleText6:: ; 8da08 (23:5a08)
-	text "Hey! You're not"
-	line "wearing shorts!"
+	text "היי! אתה לא לובש"
+	line "מכנסיים קצרים!"
 	done
 
 _Route3EndBattleText6:: ; 8da28 (23:5a28)
-	text "Lost!"
-	line "Lost! Lost!"
+	text "הפסדתי!"
+	line "אני לא מאמינה שהפסדתי!"
 	prompt
 
 _Route3AfterBattleText6:: ; 8da3b (23:5a3b)
-	text "I always wear"
-	line "shorts, even in"
-	cont "winter!"
+	text "אני תמיד לובשת"
+	line "מכנסיים קצרים,"
+	cont "אפילו בחורף!"
 	done
 
 _Route3BattleText7:: ; 8da62 (23:5a62)
-	text "You can fight my"
-	line "new #MON!"
+	text "אתה יכול להלחם"
+	line "ב#ימון החדש שלי!"
 	done
 
 _Route3EndBattleText7:: ; 8da7e (23:5a7e)
-	text "Done"
-	line "like dinner!"
+	text "בוצע"
+	line "כמו ארוחת ילדים!"
 	prompt
 
 _Route3AfterBattleText7:: ; 8da91 (23:5a91)
-	text "Trained #MON"
-	line "are stronger than"
-	cont "the wild ones!"
+	text "#ימונים מאומנים"
+	line "הינם חזקים יותר"
+	cont "מ#ימונים פראיים!"
 	done
 
 _Route3BattleText8:: ; 8dac0 (23:5ac0)
-	text "Eek! Did you"
-	line "touch me?"
+	text "איכס!"
+	line "אתה נגעת בי?"
 	done
 
 _Route3EndBattleText8:: ; 8dad8 (23:5ad8)
-	text "That's it?"
+	text "זהו זה?"
 	prompt
 
 _Route3AfterBattleText8:: ; 8dae3 (23:5ae3)
-	text "ROUTE 4 is at the"
-	line "foot of MT.MOON."
+	text "דרך 4 נמצאת במורד"
+	line "הר הירח."
 	done
 
 _Route3Text10:: ; 8db07 (23:5b07)
-	text "ROUTE 3"
-	line "MT.MOON AHEAD"
+	text "דרך 3"
+	line "בהמשך: הר ירח"
 	done
 

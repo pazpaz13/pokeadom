f8ab7ce4b3e7740850aec376a6eab073fa6e2b3f
Noarte <65566764+Noarte@users.noreply.github.com>
2020-05-19 02:16:58 +0300

    Update viridian_city.asm

--- text/maps/viridian_city.asm
+++ text/maps/viridian_city.asm
@@ -1,181 +1,177 @@
 _ViridianCityText1:: ; a43cc (29:43cc)
-	text "Those # BALLs"
-	line "at your waist!"
-	cont "You have #MON!"
+	text "אלו #דורים"
+	line "שתלויים על חגורתך!"
+	cont "אם כך, יש לך #ימונים!"
 
-	para "It's great that"
-	line "you can carry and"
-	cont "use #MON any"
-	cont "time, anywhere!"
+	para "זה נהדר שאתה"
+	line "יכול לסחוב ולהשתמש"
+	cont "ב#ימונים שלך"
+	cont "בכל זמן ומקום שתרצה!"
 	done
 
 _ViridianCityText_19122:: ; a4437 (29:4437)
-	text "This #MON GYM"
-	line "is always closed."
+	text "מכון ה#ימונים הזה"
+	line "תמיד סגור."
 
-	para "I wonder who the"
-	line "LEADER is?"
+	para "אני תוהה מי"
+	line "מנהיג המכון הזה?"
 	done
 
 _ViridianCityText_19127:: ; a4474 (29:4474)
-	text "VIRIDIAN GYM's"
-	line "LEADER returned!"
+	text "VIRIDIAN GYM's" ;TODO
+	line "LEADER חזר!"
 	done
 
 _ViridianCityText_1914d:: ; a4494 (29:4494)
-	text "You want to know"
-	line "about the 2 kinds"
-	cont "of caterpillar"
-	cont "#MON?"
+	text "רוצה ללמוד"
+	line "על 2 סוגי"
+	cont "#ימוני הזחלים"
 	done
 
 _ViridianCityText_19152:: ; a44cd (29:44cd)
-	text "Oh, OK then!"
+	text "אה, בסדר!"
 	done
 
 _ViridianCityText_19157:: ; a44db (29:44db)
-	text "CATERPIE has no"
-	line "poison, but"
-	cont "WEEDLE does."
+	text "לCATERPIE אין" ;TODO
+	line "רעל, אבל"
+	cont "לWEEDLE יש." ;TODO
 
-	para "Watch out for its"
-	line "POISON STING!"
+	para "היזהר ממתקפת ה"
+	line "POISON STING שלו!" ;TODO
 	done
 
 _ViridianCityText_19175:: ; a4525 (29:4525)
-	text "Oh Grandpa! Don't"
-	line "be so mean!"
-	cont "He hasn't had his"
-	cont "coffee yet."
+	text "הו סבתא! אל"
+	line "תהיי כה רעה!"
+	cont "הוא אפילו לא שתה"
+	cont "את הקפה שלו עדיין."
 	done
 
 _ViridianCityText_1917a:: ; a4560 (29:4560)
-	text "When I go shop in"
-	line "PEWTER CITY, I"
-	cont "have to take the"
-	cont "winding trail in"
-	cont "VIRIDIAN FOREST."
+	text "כשאני הולך לקניות"
+	line " בPEWTER CITY, אני" ;TODO
+	cont "חייב לעבור דרך"
+	cont "השביל המתפתל של"
+	cont "VIRIDIAN FOREST." ;TODO
 	done
 
 _ViridianCityText_19191:: ; a45b5 (29:45b5)
-	text "You can't go"
-	line "through here!"
+	text "אתה לא יכול ללכת"
+	line "דרך שם!"
 
-	para "This is private"
-	line "property!"
+	para "זה נכס"
+	line "פרטי!"
 	done
 
 _ViridianCityText_191ca:: ; a45ea (29:45ea)
-	text "Yawn!"
-	line "I must have dozed"
-	cont "off in the sun."
+	text "הא!"
+	line "כנראה שנרדמתי קצת בשמש."
 
-	para "I had this dream"
-	line "about a DROWZEE"
-	cont "eating my dream."
-	cont "What's this?"
-	cont "Where did this TM"
-	cont "come from?"
+	para "היה לי חלום"
+	line "על DROWZEE" ;TODO
+	cont "שאוכל את החלום שלי."
+	cont "מה זה?"
+	cont "מהיכן ה TM הזה" ;TODO
+	cont "הגיע פתאום?"
 
-	para "This is spooky!"
-	line "Here, you can"
-	cont "have this TM."
+	para "זה מפחיד!"
+	line "הנה, אתה יכול"
+	cont "לקבל את ה TM הזה." ;TODO
 	prompt
 
 _ReceivedTM42Text:: ; a469a (29:469a)
-	text $52, " received"
+	text $52, " התקבל"
 	line "TM42!@@"
 
 _TM42Explanation:: ; a46ad (29:46ad)
-	text "TM42 contains"
-	line "DREAM EATER..."
-	cont "...Snore..."
+	text "TM42 מכיל את מתקפת"
+	line "DREAM EATER..." ;TODO
+	cont "...נחירה..."
 	done
 
 _TM42NoRoomText:: ; a46d7 (29:46d7)
-	text "You have too much"
-	line "stuff already."
+	text "הגעת למכסה של החפצים"
+	line "שאתה יכול להחזיק בתיק."
 	done
 
 _ViridianCityText_1920a:: ; a46f9 (29:46f9)
-	text "Ahh, I've had my"
-	line "coffee now and I"
-	cont "feel great!"
+	text "אע, סוף סוף שתיתי"
+	line "את הקפה שלי ועכשיו"
+	cont "אני מרגיש מצוין!"
 
-	para "Sure you can go"
-	line "through!"
+	para "כמובן שאתה יכול"
+	line "לעבור כאן!"
 
-	para "Are you in a"
-	line "hurry?"
+	para "האם אתה"
+	line "ממהר?"
 	done
 
 _ViridianCityText_1920f:: ; a4754 (29:4754)
-	text "I see you're using"
-	line "a #DEX."
+	text "אני רואה שאתה משתמש ב"
+	line "שלך #DEX." ;TODO
 
-	para "When you catch a"
-	line "#MON, #DEX"
-	cont "is automatically"
-	cont "updated."
+	para "כאשר אתה תופס"
+	line "#ימון, ה#DEX"
+	cont "מתעדכן אוטומטית"
 
-	para "What? Don't you"
-	line "know how to catch"
-	cont "#MON?"
+	para "מה? אתה לא יודע"
+	line "איך לתפוס"
+	cont "#ימון?"
 
-	para "I'll show you"
-	line "how to then."
+	para "אם כך,"
+	line "אני אראה לך כיצד."
 	done
 
 _ViridianCityText_19214:: ; a47e6 (29:47e6)
-	text "Time is money..."
-	line "Go along then."
+	text "זמן זה כסף..."
+	line "המשך בדרכך."
 	done
 
 _ViridianCityText_19219:: ; a4807 (29:4807)
-	text "First, you need"
-	line "to weaken the"
-	cont "target #MON."
+	text "תחילה, אתה צריך"
+	line "להחליש את"
+	cont "ה#ימון הרצוי."
 	done
 
 _ViridianCityText8:: ; a4833 (29:4833)
-	text "VIRIDIAN CITY "
-	line "The Eternally"
-	cont "Green Paradise"
+	text "VIRIDIAN CITY " ;TODO
+	line "גן העדן הירוק"
+	cont "והנצחי"
 	done
 
 _ViridianCityText9:: ; a4860 (29:4860)
-	text "TRAINER TIPS"
+	text "TRAINER TIPS" ;TODO
 
-	para "Catch #MON"
-	line "and expand your"
-	cont "collection!"
+	para "תפוס #ימונים"
+	line "והגדל את"
+	cont "האוסף שלך!"
 
-	para "The more you have,"
-	line "the easier it is"
-	cont "to fight!"
+	para "ככל שיהיו לך יותר,"
+	line "כך יהיה לך קל יותר"
+	cont "להילחם בקרבות!"
 	done
 
 _ViridianCityText10:: ; a48c3 (29:48c3)
-	text "TRAINER TIPS"
+	text "TRAINER TIPS" ;TODO
 
-	para "The battle moves"
-	line "of #MON are"
-	cont "limited by their"
-	cont "POWER POINTs, PP."
+	para "מתקפות הלחימה"
+	line "של ה#ימונים"
+	cont "מוגבלים"
+	cont "בPOWER POINTs, PP. שלהם" ;TODO
 
-	para "To replenish PP,"
-	line "rest your tired"
-	cont "#MON at a"
-	cont "#MON CENTER!"
+	para "בכדי לחדש את מלאי ה PP," ;TODO
+	line "רפא את ה#ימון שלך"
+	cont "במרכז ה#ימונים!"
 	done
 
 _ViridianCityText13:: ; a4949 (29:4949)
-	text "VIRIDIAN CITY"
-	line "#MON GYM"
+	text "VIRIDIAN CITY" ;TODO
+	line "מכון ה#ימונים"
 	done
 
 _ViridianCityText14:: ; a4961 (29:4961)
-	text "The GYM's doors"
-	line "are locked..."
+	text "דלתות המכון"
+	line "סגורות..."
 	done
 

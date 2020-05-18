5c475213761fbc574d4595d76b295c08dbe08c90
Noarte <65566764+Noarte@users.noreply.github.com>
2020-05-19 02:28:49 +0300

    Update victory_road_2f.asm

--- text/maps/victory_road_2f.asm
+++ text/maps/victory_road_2f.asm
@@ -1,86 +1,84 @@
 _VictoryRoad2BattleText6:: ; 8d06e (23:506e)
-	text "Gyaoo!@@"
+	text "Gyaoo!@@" ;TODO
 
 _VictoryRoad2BattleText1:: ; 8d077 (23:5077)
-	text "VICTORY ROAD is"
-	line "the final test"
-	cont "for trainers!"
+	text "VICTORY ROAD היא" ;TO DO
+	line "המבחן האחרון"
+	cont "עבור מאמני #ימון!"
 	done
 
 _VictoryRoad2EndBattleText1:: ; 8d0a5 (23:50a5)
-	text "Aiyah!"
+	text "איה!"
 	prompt
 
 _VictoryRoad2AfterBattleText1:: ; 8d0ad (23:50ad)
-	text "If you get stuck,"
-	line "try moving some"
-	cont "boulders around!"
+	text "במידה ונתקעת,"
+	line "נסה להזיז כמה"
+	cont "מן הסלעים שמסביב!"
 	done
 
 _VictoryRoad2BattleText2:: ; 8d0e1 (23:50e1)
-	text "Ah, so you wish"
-	line "to challenge the"
-	cont "ELITE FOUR?"
+	text "אה, אז אתה רוצה"
+	line "לאתגר את"
+	cont "ELITE FOURה?" ;TO DO
 	done
 
 _VictoryRoad2EndBattleText2:: ; 8d10f (23:510f)
-	text "You"
-	line "got me!"
+	text "תפסת"
+	line "אותי!" 
 	prompt
 
 _VictoryRoad2AfterBattleText2:: ; 8d11c (23:511c)
-	text $53, " also came"
-	line "through here!"
+	text $53, "מגיע גם"
+	line "דרך כאן!"
 	done
 
 _VictoryRoad2BattleText3:: ; 8d137 (23:5137)
-	text "Come on!"
-	line "I'll whip you!"
+	text "קדימה!"
+	line "אני אביס אותך!"
 	done
 
 _VictoryRoad2EndBattleText3:: ; 8d14f (23:514f)
-	text "I got"
-	line "whipped!"
+	text "אני"
+	line "הובסתי!"
 	prompt
 
 _VictoryRoad2AfterBattleText3:: ; 8d15f (23:515f)
-	text "You earned the"
-	line "right to be on"
-	cont "VICTORY ROAD!"
+	text "אתה השגת"
+	line "את הזכות להגיע אל"
+	cont "VICTORY ROADה!" ;TO DO
 	done
 
 _VictoryRoad2BattleText4:: ; 8d18c (23:518c)
-	text "If you can get"
-	line "through here, you"
-	cont "can go meet the"
-	cont "ELITE FOUR!"
+	text "אם אתה ללכת"
+	line "דרך כאן אתה "
+	cont "יכול ללכת לפגוש את"
+	cont "ELITE FOURה!" ;TO DO
 	done
 
 _VictoryRoad2EndBattleText4:: ; 8d1ca (23:51ca)
-	text "No!"
-	line "Unbelievable!"
+	text "לא!"
+	line "בלתי יאומן!"
 	prompt
 
 _VictoryRoad2AfterBattleText4:: ; 8d1dd (23:51dd)
-	text "I can beat you"
-	line "when it comes to"
-	cont "knowledge about"
-	cont "#MON!"
+	text "אני יכול להביס אותך"
+	line "כשזה נוגע לידע"
+	cont "אודות ה#ימונים!"
 	done
 
 _VictoryRoad2BattleText5:: ; 8d214 (23:5214)
-	text "Is VICTORY ROAD"
-	line "too tough?"
+	text "האם ה VICTORY ROAD" ;TO DO
+	line "קשה מידי?"
 	done
 
 _VictoryRoad2EndBattleText5:: ; 8d230 (23:5230)
-	text "Well"
-	line "done!"
+	text "כל"
+	line "הכבוד!"
 	prompt
 
 _VictoryRoad2AfterBattleText5:: ; 8d23c (23:523c)
-	text "Many trainers give"
-	line "up the challenge"
-	cont "here."
+	text "הרבה מאמנים מוותרים"
+	line "על האתגר כאן"
 	done
 

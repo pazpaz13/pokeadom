5537fb194490214792dcd2d720e2e472b837a717
lalukon <65565943+lalukon@users.noreply.github.com>
2020-05-18 22:46:09 +0300

    translate route_6.asm

--- text/maps/route_6.asm
+++ text/maps/route_6.asm
@@ -1,98 +1,98 @@
 _Route6BattleText1:: ; 8dc38 (23:5c38)
-	text "Who's there?"
-	line "Quit listening in"
-	cont "on us!"
+	text "מי שם?"
+	line "בבקשה תפסיק"
+	cont "לצוטט לנו!"
 	done
 
 _Route6EndBattleText1:: ; 8dc5e (23:5c5e)
-	text "I"
-	line "just can't win!"
+	text "אני"
+	line "פשוט לא יכול לנצח!"
 	prompt
 
 _Route6AfterBattleText1:: ; 8dc70 (23:5c70)
-	text "Whisper..."
-	line "whisper..."
+	text "לחשושים.."
+	line "לחשושים..."
 	done
 
 _Route6BattleText2:: ; 8dc87 (23:5c87)
-	text "Excuse me! This"
-	line "is a private"
-	cont "conversation!"
+	text "תסלח לי!"
+	line "זו שיחה"
+	cont "פרטית למדי!"
 	done
 
 _Route6EndBattleText2:: ; 8dcb3 (23:5cb3)
-	text "Ugh!"
-	line "I hate losing!"
+	text "אוף!"
+	line "אני שונאת להפסיד!"
 	prompt
 
 _Route6BattleText3:: ; 8dcc8 (23:5cc8)
-	text "There aren't many"
-	line "bugs out here."
+	text "אין הרבה"
+	line "חרקים בסביבה."
 	done
 
 _Route6EndBattleText3:: ; 8dce9 (23:5ce9)
-	text "No!"
-	line "You're kidding!"
+	text "לא!"
+	line "אתה צוחק עליי!"
 	prompt
 
 _Route6AfterBattleText3:: ; 8dcfd (23:5cfd)
-	text "I like bugs, so"
-	line "I'm going back to"
-	cont "VIRIDIAN FOREST."
+	text "אני אוהב חרקים,"
+	line "אז אני מתכוון לחזור"
+	cont "ליער ירוק-עד."
 	done
 
 _Route6BattleText4:: ; 8dd30 (23:5d30)
-	text "Huh? You want"
-	line "to talk to me?"
+	text "המ?"
+	line "רצית לדבר איתי?"
 	done
 
 _Route6EndBattleText4:: ; 8dd4e (23:5d4e)
-	text "I"
-	line "didn't start it!"
+	text "אני"
+	line "לא התחלתי את זה!"
 	prompt
 
 _Route6AfterBattleText4:: ; 8dd61 (23:5d61)
-	text "I should carry"
-	line "more #MON with"
-	cont "me for safety."
+	text "אני צריך לשאת"
+	line "יותר #ימונים איתי"
+	cont "לבטחוני האישי."
 	done
 
 _Route6BattleText5:: ; 8dd8f (23:5d8f)
-	text "Me? Well, OK."
-	line "I'll play!"
+	text "אני? בסדר, בסדר."
+	line "אני אשחק!"
 	done
 
 _Route6EndBattleText5:: ; 8dda8 (23:5da8)
-	text "Just"
-	line "didn't work!"
+	text "זה פשוט"
+	line "לא עבד!"
 	prompt
 
 _Route6AfterBattleText5:: ; 8ddba (23:5dba)
-	text "I want to get"
-	line "stronger! What's"
-	cont "your secret?"
+	text "אני רוצה להיות"
+	line "חזקה יותר!"
+	cont "מה הסוד שלך?"
 	done
 
 _Route6BattleText6:: ; 8dde6 (23:5de6)
-	text "I've never seen"
-	line "you around!"
-	cont "Are you good?"
+	text "מעולם לא נתקלתי"
+	line "בך לפני כן!"
+	cont "כמה אתה מוצלח?"
 	done
 
 _Route6EndBattleText6:: ; 8de10 (23:5e10)
-	text "You"
-	line "are too good!"
+	text "אתה"
+	line "יותר מדי מוצלח!"
 	prompt
 
 _Route6AfterBattleText6:: ; 8de23 (23:5e23)
-	text "Are my #MON"
-	line "weak? Or, am I"
-	cont "just bad?"
+	text "אוף. האם ה#ימונים"
+	line "שלי חלשים? או שאני"
+	cont "פשוט גרוע?"
 	done
 
 _Route6Text7:: ; 8de49 (23:5e49)
-	text "UNDERGROUND PATH"
-	line "CERULEAN CITY -"
-	cont "VERMILION CITY"
+	text "דרך תת קרקעית"
+	line "עיר הטורקיז -"
+	cont "עיר הארגמן"
 	done
 

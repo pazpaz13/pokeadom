2825c231060ad36f89b1e1e592846212935d0993
Nog <nogash02@gmail.com>
2020-05-18 23:55:52 +0300

    Translate lavender_house.asm

--- text/maps/lavender_house.asm
+++ text/maps/lavender_house.asm
@@ -1,25 +1,22 @@
 _LavenderHouse2Text1:: ; 9a238 (26:6238)
-	text "CUBONE: Kyarugoo!@@"
+	text "CUBONE: Kyarugoo!@@" ; TODO
 
 _LavenderHouse2Text_1d9dc:: ; 9a24c (26:624c)
-	text "I hate those"
-	line "horrible ROCKETs!"
+	text "אני מתעבת את כנופיית" ; TODO ROCKET
+	line "רוקט האכזרית!"
 
-	para "That poor CUBONE's"
-	line "mother..."
+	para "CUBONE המסכן..." ; TODO
 
-	para "It was killed"
-	line "trying to escape"
-	cont "from TEAM ROCKET!"
+	para "אמא שלו נהרגה "
+	line "כשהיא ניסתה להימלט"
+	cont "מכנופיית רוקט!" ; TODO ROCKET
 	done
 
 _LavenderHouse2Text_1d9e1:: ; 9a2b9 (26:62b9)
-	text "The GHOST of"
-	line "#MON TOWER is"
-	cont "gone!"
+	text "הרוח שב#MON" ; TODO
+	line "TOWER נעלמה!" ; TODO
 
-	para "Someone must have"
-	line "soothed its"
-	cont "restless soul!"
+	para "מישהו כנראה"
+	line "עזר לה להירגע!"
 	done
 

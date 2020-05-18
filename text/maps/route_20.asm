4d3067dc787606861a16655cf6bcb22515cb3b8a
lalukon <65565943+lalukon@users.noreply.github.com>
2020-05-18 23:51:23 +0300

    TRANSLATE route_20.asm

--- text/maps/route_20.asm
+++ text/maps/route_20.asm
@@ -1,157 +1,156 @@
 _Route20BattleText1:: ; 91c69 (24:5c69)
-	text "The water is"
-	line "shallow here."
+	text "המים רדודים כאן."
+	line 
 	done
 
 _Route20EndBattleText1:: ; 91c85 (24:5c85)
-	text "Splash!"
+	text "ספלאש!"
 	prompt
 
 _Route20AfterBattleText1:: ; 91c8e (24:5c8e)
-	text "I wish I could"
-	line "ride my #MON."
+	text "הלוואי שיכלתי"
+	line "לרכב על ה#ימונים שלי."
 	done
 
 _Route20BattleText2:: ; 91cac (24:5cac)
-	text "SEAFOAM is a"
-	line "quiet getaway!"
+	text "איי אדווה"
+	line "זה מקום נפלא לחופשה!"
 	done
 
 _Route20EndBattleText2:: ; 91cc9 (24:5cc9)
-	text "Quit it!"
+	text "תפסיק!"
 	prompt
 
 _Route20AfterBattleText2:: ; 91cd3 (24:5cd3)
-	text "There's a huge"
-	line "cavern underneath"
-	cont "this island."
+	text "ישנה מערה ענקית"
+	line "מתחת לאי."
+	cont 
 	done
 
 _Route20BattleText3:: ; 91d01 (24:5d01)
-	text "I love floating"
-	line "with the fishes!"
+	text "אני אוהבת לצוף"
+	line "ולשחות עם הדגים!"
 	done
 
 _Route20EndBattleText3:: ; 91d23 (24:5d23)
-	text "Yowch!"
+	text "אווץ'!"
 	prompt
 
 _Route20AfterBattleText3:: ; 91d2b (24:5d2b)
-	text "Want to float"
-	line "with me?"
+	text "רוצה לצוף"
+	line "לצידי?"
 	done
 
 _Route20BattleText4:: ; 91d43 (24:5d43)
-	text "Are you on"
-	line "vacation too?"
+	text "גם אתה כאן בחופשה?"
+	line 
 	done
 
 _Route20EndBattleText4:: ; 91d5d (24:5d5d)
-	text "No"
-	line "mercy at all!"
+	text "ללא רחמים!"
+	line 
 	prompt
 
 _Route20AfterBattleText4:: ; 91d6f (24:5d6f)
-	text "SEAFOAM used to"
-	line "be one island!"
+	text "איי אדווה"
+	line "היו פעם 'אי אדווה'"
 	done
 
 _Route20BattleText5:: ; 91d8f (24:5d8f)
-	text "Check out my buff"
-	line "physique!"
+	text "שם לב"
+	line "למבנה הגוף המרשים שלי!"
 	done
 
 _Route20EndBattleText5:: ; 91dac (24:5dac)
-	text "Wimpy!"
+	text "Wimpy!" ; TODO
 	prompt
 
 _Route20AfterBattleText5:: ; 91db4 (24:5db4)
-	text "I should've been"
-	line "buffing up my"
-	cont "#MON, not me!"
+	text "הייתי צריך"
+	line "לפתח שרירים ל#ימונים"
+	cont "שלי, לא לעצמי!"
 	done
 
 _Route20BattleText6:: ; 91de1 (24:5de1)
-	text "Why are you"
-	line "riding a #MON?"
-	cont "Can't you swim?"
+	text "למה אתה רוכב"
+	line "על #ימון?"
+	cont "אתה לא יודע לשחות?"
 	done
 
 _Route20EndBattleText6:: ; 91e0c (24:5e0c)
-	text "Ouch!"
-	line "Torpedoed!"
+	text "אווץ'!"
+	line "טורפדו!!"
 	prompt
 
 _Route20AfterBattleText6:: ; 91e1e (24:5e1e)
-	text "Riding a #MON"
-	line "sure looks fun!"
+	text "רכיבה על #ימונים בהחלט"
+	line "נראית כמו כיף גדול!"
 	done
 
 _Route20BattleText7:: ; 91e3d (24:5e3d)
-	text "I rode my bird"
-	line "#MON here!"
+	text "רכבתי על #ימון"
+	line "הציפור שלי לכאן!"
 	done
 
 _Route20EndBattleText7:: ; 91e58 (24:5e58)
-	text "Oh"
-	line "no!"
+	text "אוי!"
+	line "לא!"
 	prompt
 
 _Route20AfterBattleText7:: ; 91e60 (24:5e60)
-	text "My birds can't"
-	line "FLY me back!"
+	text "הציפורים שלי לא יכולות"
+	line "לשאת אותי בחזרה!"
 	done
 
 _Route20BattleText8:: ; 91e7c (24:5e7c)
-	text "My boy friend gave"
-	line "me big pearls!"
+	text "בן הזוג שלי הביא לי"
+	line "פנינים גדולות ויפות!"
 	done
 
 _Route20EndBattleText8:: ; 91e9f (24:5e9f)
-	text "Don't"
-	line "touch my pearls!"
+	text "שלא תעז"
+	line "לגעת בפנינים שלי!"
 	prompt
 
 _Route20AfterBattleText8:: ; 91eb6 (24:5eb6)
-	text "Will my pearls"
-	line "grow bigger"
-	cont "inside CLOYSTER?"
+	text "האם הפנינים שלי"
+	line "יהפכו לגדולות יותר"
+	cont "בתוך CLOYSTER?" ;TODO
 	done
 
 _Route20BattleText9:: ; 91ee3 (24:5ee3)
-	text "I swam here from"
-	line "CINNABAR ISLAND!"
+	text "שחיתי לכאן"
+	line "כל הדרך מאי הצנובר!"
 	done
 
 _Route20EndBattleText9:: ; 91f06 (24:5f06)
-	text "I'm"
-	line "so disappointed!"
+	text "אני"
+	line "כל כך מאוכזבת!"
 	prompt
 
 _Route20AfterBattleText9:: ; 91f1b (24:5f1b)
-	text "#MON have"
-	line "taken over an"
-	cont "abandoned mansion"
-	cont "on CINNABAR!"
+	text "#ימונים השתלטו"
+	line "על אחוזה נטושה"
+	cont "באי הצנובר!"
 	done
 
 _Route20BattleText10:: ; 91f53 (24:5f53)
-	text "CINNABAR, in the"
-	line "west, has a LAB"
-	cont "for #MON."
+	text "בצנובר, האי במערב,"
+	line "יש מעבדת #ימונים."
+	cont 
 	done
 
 _Route20EndBattleText10:: ; 91f7f (24:5f7f)
-	text "Wait!"
+	text "חכה!"
 	prompt
 
 _Route20AfterBattleText10:: ; 91f86 (24:5f86)
-	text "CINNABAR is a "
-	line "volcanic island!"
+	text "צנובר"
+	line "הוא אי געשי!"
 	done
 
 _Route20Text12:: ; 91fa7 (24:5fa7)
 _Route20Text11:: ; 91fa7 (24:5fa7)
-	text "SEAFOAM ISLANDS"
+	text "איי אדווה"
 	done
 

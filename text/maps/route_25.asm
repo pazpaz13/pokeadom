8bb2c14a2a2faf74cbbe2150cfd424b845211822
lalukon <65565943+lalukon@users.noreply.github.com>
2020-05-19 00:19:48 +0300

    TRANSLATE route_25.asm

--- text/maps/route_25.asm
+++ text/maps/route_25.asm
@@ -1,157 +1,157 @@
 _Route25BattleText1:: ; 9421e (25:421e)
-	text "Local trainers"
-	line "come here to"
-	cont "practice!"
+	text "Local trainers" ; TODO 
+	line "בואו הנה"
+	cont "והתאמנו!"
 	done
 
 _Route25EndBattleText1:: ; 94245 (25:4245)
-	text "You're"
-	line "decent."
+	text "אתה"
+	line "לא רע."
 	prompt
 
 _Route25AfterBattleText1:: ; 94254 (25:4254)
-	text "All #MON have"
-	line "weaknesses. It's"
-	cont "best to raise"
-	cont "different kinds."
+	text "לכל #ימון"
+	line "יש חולשה."
+	cont "לכן מומלץ לגדל"
+	cont "סוגים שונים."
 	done
 
 _Route25BattleText2:: ; 94292 (25:4292)
-	text "Dad took me to a"
-	line "great party on"
-	cont "S.S.ANNE at"
-	cont "VERMILION CITY!"
+	text "אבא לקח אותי"
+	line "למסיבה נהדרת"
+	cont "על הספינה אנה"
+	cont "בעיר הארגמן!"
 	done
 
 _Route25EndBattleText2:: ; 942cf (25:42cf)
-	text "I'm"
-	line "not mad!"
+	text "אני"
+	line "לא כועס!" ; TODO 'משוגע?'
 	prompt
 
 _Route25AfterBattleText2:: ; 942dc (25:42dc)
-	text "On S.S.ANNE, I"
-	line "saw trainers from"
-	cont "around the world."
+	text "בספינה אנה"
+	line "פגשתי מאמנים"
+	cont "מכל רחבי תבל."
 	done
 
 _Route25BattleText3:: ; 94310 (25:4310)
-	text "I'm a cool guy."
-	line "I've got a girl"
-	cont "friend!"
+	text "אני בחור מדליק."
+	line "יש לי בת זוג!"
+	cont 
 	done
 
 _Route25EndBattleText3:: ; 94337 (25:4337)
-	text "Aww,"
-	line "darn..."
+	text "אויש,"
+	line "נו..."
 	prompt
 
 _Route25AfterBattleText3:: ; 94345 (25:4345)
-	text "Oh well. My girl"
-	line "will cheer me up."
+	text "טוב נו. בת הזוג"
+	line "שלי תעודד אותי."
 	done
 
 _Route25BattleText4:: ; 94369 (25:4369)
-	text "Hi! My boy"
-	line "friend is cool!"
+	text "היי! גם בן הזוג"
+	line "שלי מגניב!"
 	done
 
 _Route25EndBattleText4:: ; 94385 (25:4385)
-	text "I was in"
-	line "bad condition!"
+	text "אני הייתי"
+	line "במצב רעוע!"
 	prompt
 
 _Route25AfterBattleText4:: ; 9439e (25:439e)
-	text "I wish my guy was"
-	line "as good as you!"
+	text "הלוואי שהבחור שלי"
+	line "היה טוב כמוך!"
 	done
 
 _Route25BattleText5:: ; 943c1 (25:43c1)
-	text "I knew I had to"
-	line "fight you!"
+	text "ידעתי שהייתי חייב"
+	line "להלחם בך!"
 	done
 
 _Route25EndBattleText5:: ; 943dd (25:43dd)
-	text "I knew"
-	line "I'd lose too!"
+	text "גם ידעתי"
+	line "שאני אפסיד!"
 	prompt
 
 _Route25AfterBattleText5:: ; 943f2 (25:43f2)
-	text "If your #MON"
-	line "gets confused or"
-	cont "falls asleep,"
-	cont "switch it!"
+	text "אם ה#ימון שלך"
+	line "gets confused or" ; TODO
+	cont "falls asleep," ; TODO
+	cont "תחליף אותו!"
 	done
 
 _Route25BattleText6:: ; 9442a (25:442a)
-	text "My friend has a"
-	line "cute #MON."
-	cont "I'm so jealous!"
+	text "לחבר שלי יש"
+	line "#ימון כזה מתוק!"
+	cont "אני כל כך מקנאה!"
 	done
 
 _Route25EndBattleText6:: ; 94455 (25:4455)
-	text "I'm"
-	line "not so jealous!"
+	text "אני לא"
+	line "מקנאה עד כדי כך!"
 	prompt
 
 _Route25AfterBattleText6:: ; 94469 (25:4469)
-	text "You came from MT."
-	line "MOON? May I have"
-	cont "a CLEFAIRY?"
+	text "אתה הגעת מהר הירח?"
+	line "תוכל להביא לי"
+	cont "a CLEFAIRY?" ; TODO
 	done
 
 _Route25BattleText7:: ; 94499 (25:4499)
-	text "I just got down"
-	line "from MT.MOON,"
-	cont "but I'm ready!"
+	text "בדיוק חזרתי"
+	line "מהר הירח,"
+	cont "אבל אני מוכן!"
 	done
 
 _Route25EndBattleText7:: ; 944c6 (25:44c6)
-	text "You"
-	line "worked hard!"
+	text "אתה"
+	line "עבדת קשה!"
 	prompt
 
 _Route25AfterBattleText7:: ; 944d8 (25:44d8)
-	text "Drat!"
-	line "A ZUBAT bit me"
-	cont "back in there."
+	text "אוף!"
+	line "A ZUBAT bit me" ; TODO
+	cont "back in there." ; TODO
 	done
 
 _Route25BattleText8:: ; 944fd (25:44fd)
-	text "I'm off to see a"
-	line "#MON collector"
-	cont "at the cape!"
+	text "אני הולך לפגוש"
+	line "אספן #ימונים."
+	cont "הוא מחכה במזח!"
 	done
 
 _Route25EndBattleText8:: ; 9452a (25:452a)
-	text "You"
-	line "got me."
+	text "שכנעת אותי."
+	line 
 	prompt
 
 _Route25AfterBattleText8:: ; 94537 (25:4537)
-	text "The collector has"
-	line "many rare kinds"
-	cont "of #MON."
+	text "לאספן יש"
+	line "כל מיני סוגים"
+	cont "של #ימונים נדירים!"
 	done
 
 _Route25BattleText9:: ; 94563 (25:4563)
-	text "You're going to"
-	line "see BILL? First,"
-	cont "let's fight!"
+	text "אתה הולך לראות"
+	line "את BILL?" ; TODO
+	cont "קודם, בוא נלחם!"
 	done
 
 _Route25EndBattleText9:: ; 94590 (25:4590)
-	text "You're"
-	line "something."
+	text "אתה"
+	line "משהו מיוחד."
 	prompt
 
 _Route25AfterBattleText9:: ; 945a2 (25:45a2)
-	text "The trail below"
-	line "is a shortcut to"
-	cont "CERULEAN CITY."
+	text "השביל למטה"
+	line "הוא קיצור דרך"
+	cont "לעיר הטורקיז."
 	done
 
 _Route25Text11:: ; 945d3 (25:45d3)
-	text "SEA COTTAGE"
-	line "BILL lives here!"
+	text "SEA COTTAGE" ; TODO
+	line "BILL גר כאן!" ; TODO
 	done
 

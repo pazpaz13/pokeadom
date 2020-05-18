c63fe9339d1bd68a08c408702b36393152018bba
Noarte <65566764+Noarte@users.noreply.github.com>
2020-05-19 00:56:01 +0300

    Update viridian_forest_entrance.asm

--- text/maps/viridian_forest_entrance.asm
+++ text/maps/viridian_forest_entrance.asm
@@ -1,14 +1,14 @@
 _ViridianForestEntranceText1:: ; 8a868 (22:6868)
-	text "Are you going to"
-	line "VIRIDIAN FOREST?"
-	cont "Be careful, it's"
-	cont "a natural maze!"
+	text "אתה הולך אל"
+	line "יער ורידיאן?"
+	cont "תיזהר, היער הזה"
+	cont "הוא ממש כמו מבוך!"
 	done
 
 _ViridianForestEntranceText2:: ; 8a8ab (22:68ab)
-	text "RATTATA may be"
-	line "small, but its"
-	cont "bite is wicked!"
-	cont "Did you get one?"
+	text "רטטה אולי"
+	line "קטן, אבל הנשיכה שלו"
+	cont "שלו כל כך כואבת!"
+	cont "חטפת אחת?"
 	done
 
